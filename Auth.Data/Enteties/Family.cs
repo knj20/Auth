85215f0d19736b381988c7135b50c119cb55aa0c
Karima El Khayati <kelkhaya@emeal.nttdata.com>
2022-09-16 22:53:26 +0100

    first commit

---
+++ Auth.Data/Enteties/Family.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Data.Enteties
+{
+    class Family
+    {
+        /// <summary>
+        /// gets or sets the Family id.
+        /// </summary>
+        Guid Id { get; set; }
+
+        /// <summary>
+        /// gets or sets the name of the Family.
+        /// </summary>
+        string Name { get; set; }
+    }
+}
---
+++ Auth.Data/Enteties/Plant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Data.Enteties
+{
+    class Plant
+    {
+        /// <summary>
+        /// gets or sets the plant id.
+        /// </summary>
+        Guid Id { get; set; }
+
+        /// <summary>
+        /// gets or sets the name of the plant.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// gets or sets the available of the plant.
+        /// </summary>
+        bool Available { get; set; }
+
+        /// <summary>
+        /// gets or sets the price of the plant.
+        /// </summary>
+        double Price { get; set; }
+
+        /// <summary>
+        /// gets or sets the family of the plant.
+        /// </summary>
+        Family Family { get; set; }
+
+
+    }
+}
